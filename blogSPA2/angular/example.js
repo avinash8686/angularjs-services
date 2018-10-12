a71672a9e099926d179be84c46da4a1b309da5d2
AviAvinash <avinash.dv.264@gmail.com>
2018-10-12 23:03:44 +0530

    angular js services

---
+++ blogSPA2/angular/example.js
@@ -0,0 +1,21 @@
+
+var myApp = angular.module("blogApp", []);
+
+// in normal cases, controller is just a function
+myApp.controller('mainController',function(){
+
+
+});
+
+// in case of service, 
+
+myApp.controller('mainController',['$http','$location',function($http,$location){
+
+
+
+
+
+
+
+
+}])
